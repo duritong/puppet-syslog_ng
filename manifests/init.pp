7dafa8b1bd9555fc2fc4cd176d9cb7fdcb7d08fc
andreas <andreas@immerda.ch>
2008-01-25 14:26:21 +0000

    config file for syslog-ng

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,6 +15,14 @@ class syslog-ng {
 	
 	service { syslog-ng: ensure  => running, enable  => true, }
 
+    file { "/etc/syslog-ng/syslog-ng.conf":
+        source => "puppet://$servername/syslog-ng/syslog-ng.conf",
+        ensure => file,
+        force => true,
+        mode => 0644, owner => root, group => 0;
+
+    }
+
 	package { metalog:
                 name => $operatingsystem ? {
                         centos => 'metalog',
